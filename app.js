680e8ceaee0742c4228a57a7b981a04d7e06b6a9
Xavier-Hsiao <tim88776@gmail.com>
2021-09-28 23:01:40 +0800

    完成show頁面路由和搜尋功能
    1. 下載餐廳jason檔，並載入專案中，設定index.handlebars用each迴圈渲染出所有餐廳
    2. 打造show頁面的路由，呈現特定餐廳的詳細頁面
    3. 完成搜尋功能(Query String)，給keyword加上split().join()，提升搜尋體驗

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
+//9/28--> 利用靜態資料渲染index的餐廳頁面
+//9/28--> 打造show頁面的路由和畫面
+//9/28--> 設定搜尋功能(query string)
+
 //將 Express 和其他 package 導入專案
 const express = require('express')
 const app = express()
 const exphbs = require('express-handlebars')
+const restoList = require('./restaurant.json')
 
 //設定伺服器相關變數
 const port = 3000
@@ -15,7 +20,21 @@ app.use(express.static('public'))
 
 //路由
 app.get('/', (req, res) => {
-  res.render('index')
+  res.render('index', {restos: restoList.results})
+})
+
+app.get('/restaurants/:id', (req, res) => {
+  // console.log(req.params)
+  const resto = restoList.results.find(restaurant => restaurant.id === Number(req.params.id))
+  res.render('show', {resto})
+})
+
+app.get('/search', (req, res) => {
+  // console.log(req.query)
+  const keyword = req.query.keyword.toLocaleLowerCase().split(' ').join('')
+  console.log(keyword)
+  const restos = restoList.results.filter(resto => resto.name.toLocaleLowerCase().includes(keyword) || resto.category.toLocaleLowerCase().toLocaleLowerCase().includes(keyword))
+  res.render('index', {restos, keyword})
 })
 
 //監聽通訊阜3000以啟動伺服器
