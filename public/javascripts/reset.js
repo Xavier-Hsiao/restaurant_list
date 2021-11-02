85c12106d48de18cf952219d9b5c83b83b24d8e7
Xavier-Hsiao <tim88776@gmail.com>
2021-11-02 23:11:52 +0800

    feat: 新增編輯頁面的reset按鈕但失敗

---
+++ public/javascripts/reset.js
@@ -0,0 +1,11 @@
+const resetButton = document.querySelector('.btn-reset')
+const formControl = document.querySelectorAll('.form-control')
+
+resetButton.addEventListener('click', function onClickedRest() {
+  formControl.forEach(input => {
+  input.value = ''
+  console.log(input)
+  })
+  console.log(formControl)
+})
+
